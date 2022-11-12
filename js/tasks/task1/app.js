3190b5c86c954643f28d02ec9902537be059a25f
ulvieyvazov <eyvazovulvi010101@gmail.com>
2022-11-12 10:39:42 +0300

    add task1

---
+++ js/tasks/task1/app.js
@@ -0,0 +1,20 @@
+//alert("Salam hamiya")
+
+let width = prompt('duzbucaqlinin enini daxil edin')
+let height = prompt('duzbucaqlinin uzunlugunu daxil edin')
+let sahe = width * height
+ console.log('duzbucaqlinin sahesi: ' + sahe);
+
+
+ let user = prompt('dogum tarixinizi daxil edin')
+let yas = (2022 - user)
+ console.log( 'sizin yasiniz: ' + yas);
+
+
+
+
+let kilometers = prompt('kilometri daxil edin')
+let mil = (kilometers *  0.6214);
+console.log('mil deyeri= ' + mil);
+alert(kilometers * 0.6214)
+
