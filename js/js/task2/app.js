fc4e21c69e9c9b5d50b38e0357c0d6277f54c875
ulvieyvazov <eyvazovulvi010101@gmail.com>
2022-11-12 13:44:11 +0300

    add task2

---
+++ js/js/task2/app.js
@@ -0,0 +1,20 @@
+let number1 = Number(prompt('birinci ededi daxil edin'))
+let number2 = Number(prompt('ikinci ededi daxil edin'))
+
+let total = (number1 + number2)
+console.log(`${number1}+${number2}= ${total}`);
+
+let difference = (number1 - number2)
+console.log(`${number1}-${number2}= ${difference}`);
+
+let hasil = (number1 * number2)
+console.log(`${number1}*${number2}= ${hasil}`);
+
+let nisbet = (number1 / number2)
+console.log(`${number1}/${number2}= ${nisbet}`);
+
+
+let celsius = Number(prompt('selsi deyerini daxil edin'))
+let fahrenheit = (celsius * 1.8 + 32)
+console.log('farenheyt deyeri: '+ fahrenheit);
+
