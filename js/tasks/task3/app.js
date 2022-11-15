c2d1cbda92531de9bcdfdf766641e6bc2bb82e29
ulvieyvazov <eyvazovulvi010101@gmail.com>
2022-11-15 16:21:26 +0300

    add task3

---
+++ js/tasks/task3/app.js
@@ -0,0 +1,51 @@
+let eded = prompt('ededi daxil edin')
+let kv_kok = (eded ** (1/2))
+console.log('ededin kavadrat koku: '+kv_kok);
+
+
+let number = Number(prompt('ededi daxil edin'))
+if (number > 0){
+    console.log('eded musbetdir');
+}
+else if(number < 0){
+    console.log('eded menfidir');
+}
+else {
+    console.log('eded sifira beraberdir');
+    
+}
+
+
+let tek_cut = prompt('ededi daxil edin')
+if (tek_cut % 2 == 0){
+    console.log('eded cutdur');
+}
+else{
+    console.log('eded tekdir');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
