78431950fa6ebd1783ede602ff1866f038637b02
ulvieyvazov <eyvazovulvi010101@gmail.com>
2022-11-22 18:59:15 +0300

    add task 4

---
+++ js/tasks/task4/app.js
@@ -0,0 +1,27 @@
+// let user = +prompt('eded daxil edin')
+// let cem = 0
+// for(a = 1; a <=user ; a++){  
+//     cem+=a
+
+// }
+// console.log(`birden ${user} daxil olmaqla ededlerin cemi: `+ cem);
+
+
+// let hasil = 1
+// let user = +prompt('eded daxil edin')
+// for (i = 1; i <= user; i++) {
+//     hasil *= i
+
+// }
+// console.log('factorial: '+hasil )
+
+
+// let a = 1
+// let user = +prompt('ededi daxil edin')
+// for(; a<=user; a++){
+//     for( let b = 0 ; b<=a; b++ ){
+//         document.write('*')
+//     }
+//     document.write('<br/>')
+// }
+
