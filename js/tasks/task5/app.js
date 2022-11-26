d6ff48c2e9ba7acb579d5d0b9c68bd4c79ad34ba
ulvieyvazov <eyvazovulvi010101@gmail.com>
2022-11-26 14:17:51 +0300

    add task5

---
+++ js/tasks/task5/app.js
@@ -0,0 +1,37 @@
+function ededler(value){
+    if(value>2 && value % 2 === 0){
+        console.log('eded murekkebdir');
+    }
+    else{
+        console.log('eded sadedir');
+    }
+}
+ededler(2)
+
+//----------------------------------------------------------------------------
+
+
+function dereceler(selsi) {
+    return faranheit = selsi * 1.8 + 32;
+}
+dereceler(8)
+console.log(faranheit);
+
+//----------------------------------------------------------------------------
+
+function cevre(radius) {
+    return sahe = Math.PI * radius * radius
+}
+cevre(5)
+console.log(sahe);
+
+//----------------------------------------------------------------------------
+
+function eded(tesadufi){
+    return Math.floor(Math.random() * tesadufi)   
+}
+const getRandom = eded(20)
+console.log(getRandom);
+
+
+
