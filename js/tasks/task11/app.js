bba5c516b644a598196d648a56b036c1306f7863
ulvieyvazov <eyvazovulvi010101@gmail.com>
2022-12-15 22:46:02 +0300

    add task11

---
+++ js/tasks/task11/app.js
@@ -0,0 +1,22 @@
+let eded = +prompt('eded daxil edin')
+let eded1 = +prompt('eded daxil edin')
+let eded2 = +prompt('eded daxil edin')
+let eded3 = +prompt('eded daxil edin')
+let eded4 = +prompt('eded daxil edin')
+const ededler = [eded, eded1, eded2, eded3, eded4,]
+ededler.length = 5
+
+function mul(HASİL) {
+    HASİL = eded * eded1 * eded2 * eded3 * eded4
+    console.log('HASILI:' + HASİL);
+}
+mul()
+
+//--------------------------------------------------
+function multiply(a, b, c) {
+    const sum = [a, b, c]
+    cemi = a * b * c
+    console.log(sum);
+    console.log(cemi);
+}
+multiply(5, 7, 3)
